0f89d9f55b5d00bb83297d39e27af3232e74efb9
Andrey <a89615076005@yandex.ru>
2022-09-24 23:40:21 +0300

    changes in task 13

--- Task13_ThirdNumber/Program.cs
+++ Task13_ThirdNumber/Program.cs
@@ -3,10 +3,17 @@ Console.Clear();
 
 Console.Write("Введите число: ");
 int number = int.Parse(Console.ReadLine());
+number = Math.Abs(number);
 
-    while (number / 1000 > 0)
+if (number > 99) 
+{
+    while (number > 999)
+    {
         number = number / 10;
-    if (number / 100 <= 0)
-        Console.WriteLine("Третьей цифры нет");
-    else
-        Console.WriteLine($"Третья цифра - {number % 10}");
+    } 
+    Console.WriteLine ($"Третья цифра = {number % 10}");   
+}
+else
+{
+    Console.WriteLine ("Третьей цифры нет");
+}
