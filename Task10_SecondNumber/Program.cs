6fa5dd21f758b85d521c3245f3030e5c01eb7ee9
Andrey <a89615076005@yandex.ru>
2022-09-24 22:56:19 +0300

    Add Task 10

---
+++ Task10_SecondNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿//  Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+
+Console.Write("Введите целое трехзначное число: ");
+int number = int.Parse(Console.ReadLine());
+
+int result = (number % 100) / 10;
+Console.WriteLine(result);
